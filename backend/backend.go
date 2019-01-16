14c6a5496278f8f04f94f5992128c56eeca92cea
Albin Kerouanton <stratege3@gmail.com>
2019-01-16 15:44:35 +0100

    Fix tabs in backend module

--- backend/backend.go
+++ backend/backend.go
@@ -1,36 +1,54 @@
 package backend
 
 import (
-  "context"
+	"context"
+	"fmt"
 
-  "github.com/NiR-/prom-autoexporter/models"
+	"github.com/NiR-/prom-autoexporter/models"
 )
 
-interface Backend {
-  RunExporter(context.Context, models.Exporter) error
-  FindMissingExporters(context.Context) error
-  CleanupExporters(context.Context, bool) error
-  CleanupExporter(context.Context, string, bool) error
-  GetPromStaticConfig(context.Context) (*models.StaticConfig, error)
-  ListenForTasksToExport(context.Context) (chan models.Exporter, chan error)
+type Backend interface{
+	RunExporter(context.Context, models.Exporter) error
+	FindMissingExporters(context.Context) []models.Exporter
+	CleanupExporters(context.Context, bool) error
+	CleanupExporter(context.Context, string, bool) error
+	GetPromStaticConfig(context.Context) (*models.StaticConfig, error)
+	ListenForTasksToExport(context.Context) (chan models.Exporter, chan error)
 }
 
-interface Event {
-  Exporter() models.Exporer
+type Event interface{
+	Exporter() models.Exporter
 }
 
 type TaskToExportStarted struct {
-  exporter models.Exporter
+	exporter models.Exporter
 }
 
 func (evt TaskToExportStarted) Exporter() models.Exporter {
-  return evt.exporter
+	return evt.exporter
 }
 
 type TaskToExportStopped struct {
-  exporter models.Exporter
+	exporter models.Exporter
 }
 
 func (evt TaskToExportStopped) Exporter() models.Exporter {
-  return evt.exporter
+	return evt.exporter
+}
+
+type errTaskToExportNotFound struct{
+	taskName string
+}
+
+func (e errTaskToExportNotFound) Error() string {
+	return fmt.Sprintf("task to export %q not found", e.taskName)
+}
+
+func NewErrTaskToExportNotFound(taskName string) error {
+	return errTaskToExportNotFound{taskName}
+}
+
+func IsErrTaskToExportNotFound(e error) bool {
+	_, ok := e.(errTaskToExportNotFound)
+	return ok
 }
